375d0bd5d347c637b9f326cc7bf6c23f95109e92
mhelgestad <maxhelgestad6@outlook.com>
2023-01-23 20:20:55 +0000

    formatters

---
+++ stringformatchallenge.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    const uri = "https://example.org:6001/v2/snacks?"
+
+    var r, q, s string = "req=snickers", "quantity=1", "size=king"
+
+    res := fmt.Sprintf("%s%s&%s&%s", uri, r, q, s)
+    fmt.Println(res)
+}
